602ed5f590553e05609b0a1b69e5e070de16931f
Unknown <rasarex@o2.pl>
2019-01-09 09:44:56 +0100

    Fixing small bugs

--- src/main/java/unislask/Author.java
+++ src/main/java/unislask/Author.java
@@ -1,6 +1,6 @@
 package unislask;
 
-import java.util.HashMap;;
+import java.util.HashMap;
 
 class Author {
 	HashMap<String, String> attributes;
--- src/main/java/unislask/BookSeries.java
+++ src/main/java/unislask/BookSeries.java
@@ -1,6 +1,4 @@
 package unislask;
 
-;
-
 class BookSeries {
 }
